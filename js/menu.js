86480df568863f1bc2edc6a172137cbe39add061
Vallek <vforval86@gmail.com>
2021-12-04 13:48:27 +0300

    сверстал и оживил попап меню

---
+++ js/menu.js
@@ -0,0 +1,27 @@
+// Находим нужные узлы
+const popupButton = document.querySelector('.popup-menu__button');
+const popupWrapper = document.querySelector('.popup-menu');
+const popupMenu = document.querySelector('.popup-menu__list');
+
+// Выполняем функцию когда юзер кликает по кнопке
+popupButton.addEventListener('click', handleMenu);
+
+// Функция, которая показывает и скрывает попап
+function handleMenu() {
+	popupMenu.classList.toggle('display-none');
+}
+
+// Выполняем функцию когда юзер кликает в любом месте
+document.addEventListener('click', hidePopup);
+
+function hidePopup(el) {
+	// Проверяем, есть ли внутри того по чему кликнули попап меню и кнопка
+	let targetInside = popupWrapper.contains(el.target);
+	// Если нет, то прячем меню
+	if (!targetInside) {
+		popupMenu.classList.add('display-none');
+	}	
+	else {
+		return;
+	}
+}
